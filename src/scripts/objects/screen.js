a9e030082789cc9b459af0761d52e180e5619482
Moiseis-Cruz <moizzito@gmail.com>
2023-06-26 14:22:24 -0300

    Coloquei os objetos extras do repositories.

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -4,22 +4,28 @@ const screen = {
         this.userProfile.innerHTML = `<div class="info">
             <img src="${user.avatarUrl}" alt="Foto do perfil do usuário" />
                             <div class="data">
-                                <h1>${user.name ?? 'Não possui nome cadastrado!!!'}</h1>
-                                <p>${user.bio ?? 'Não possui bio cadastrada!!!'}</p>
+                                <h1>${user.name ?? 'Não possui nome cadastrado! 😢'}</h1>
+                                <p>${user.bio ?? 'Não possui bio cadastrada! 😢'}</p>
                                 <p>Seguidores: ${user.followers}</p>
                                 <p>Seguindo: ${user.following}</p>
                             </div>
         </div>`
 
         let repositoriesItems = ''
-        user.repositories.forEach(repo => repositoriesItems += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`);
+        user.repositories.forEach(repo => repositoriesItems += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a> <div>Forks: 🍴 ${repo.forks} Stars: ⭐ ${repo.stargazers_count} Watchers: 👀 ${repo.watchers} Language: 👨‍💻 ${repo.language}</div></li>`);
 
+        
         if(user.repositories.length > 0){
             this.userProfile.innerHTML += `<div class="repositories section">
-                <h2>Repositórios</h2>
-                <ul>${repositoriesItems}</ul>
+            <h2>Repositórios</h2>
+            <ul>${repositoriesItems}</ul>
             </div>`
         }
+        
+        // let userEvents = ''
+        // user.events.forEach(e => userEvents += `<li>${e.events}</li>`);
+        // let eventsItem = ''
+        
     },
     renderNotFound(){
         this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
@@ -27,3 +33,5 @@ const screen = {
 }
 
 export {screen}
+
+// repositories: [forks: '', stargazers_count: '', watchers: '', language: ''],
