88616f5bc6aa75102f17cbdbc92846330cbcd0c6
Moiseis-Cruz <moizzito@gmail.com>
2023-06-26 21:40:58 -0300

    Observar consoles e comentários no código.

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -4,5 +4,6 @@ async function getEvents(userName) {
     const response = await fetch(`${baseUrl}/${userName}/events?per_page=10`)
     return await response.json()
 }
+console.log(await getEvents('moiseis-cruz'));
 
 export {getEvents}
