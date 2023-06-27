e806342850a6e407773ddc4564416f2b1b96cf7b
Moiseis-Cruz <moizzito@gmail.com>
2023-06-27 14:10:46 -0300

    Apaguei comentários do events.js

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -4,6 +4,5 @@ async function getEvents(userName) {
     const response = await fetch(`${baseUrl}/${userName}/events?per_page=10`)
     return await response.json()
 }
-console.log(await getEvents('moiseis-cruz'));
 
 export {getEvents}
