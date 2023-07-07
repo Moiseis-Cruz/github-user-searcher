187c8e07e1f80a1cf51580c11ddd5709d11b0fe7
Moiseis-Cruz <moizzito@gmail.com>
2023-07-06 23:18:19 -0300

    Mexi pouca coisa.

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -6,8 +6,8 @@ const screen = {
                             <div class="data">
                                 <h1>${user.name ?? 'Não possui nome cadastrado! 😢'}</h1>
                                 <p>${user.bio ?? 'Não possui bio cadastrada! 😢'}</p>
-                                <p>👥️️Seguidores: ${user.followers}</p>
-                                <p>👤Seguindo: ${user.following}</p>
+                                <p>👥️️ ${user.followers} Seguidores</p>
+                                <p>👤 ${user.following} Seguindo</p>
                             </div>
         </div>`
 
