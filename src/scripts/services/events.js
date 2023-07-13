7927093e7071fe1506ccea7ff6b74bf8e9c9f2d1
Moiseis-Cruz <moizzito@gmail.com>
2023-07-12 21:11:36 -0300

    Adicionei a variável ao fetch.

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -1,7 +1,7 @@
-import { baseUrl } from "../variables.js";
+import { baseUrl, quantity } from "../variables.js";
 
 async function getEvents(userName) {
-    const response = await fetch(`${baseUrl}/${userName}/events?per_page=10`)
+    const response = await fetch(`${baseUrl}/${userName}/events?per_page=${quantity}`)
     return await response.json()
 }
 
