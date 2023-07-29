4d2f4dee3679632c777001ff0116014e16242890
moizzito@gmail.com <moizzito@gmail.com>
2023-07-29 10:27:08 -0300

    Indentação do código.

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,24 +1,30 @@
 const screen = {
     userProfile: document.querySelector(".profile-data"),
     renderUser(user){
-        this.userProfile.innerHTML = `<div class="info">
-            <img src="${user.avatarUrl}" alt="Foto do perfil do usuário" />
-                            <div class="data">
-                                <h1>${user.name ?? 'Não possui nome cadastrado! 😢'}</h1>
-                                <p>${user.bio ?? 'Não possui bio cadastrada! 😢'}</p>
-                                <p>👥️️ ${user.followers} Seguidores</p>
-                                <p>👤 ${user.following} Seguindo</p>
-                            </div>
-        </div>`
+        this.userProfile.innerHTML = `  <div class="info">
+                                            <img src="${user.avatarUrl}" alt="Foto do perfil do usuário" />
+                                                <div class="data">
+                                                    <h1>${user.name ?? 'Não possui nome cadastrado! 😢'}</h1>
+                                                    <p>${user.bio ?? 'Não possui bio cadastrada! 😢'}</p>
+                                                    <p>👥️️ ${user.followers} Seguidores</p>
+                                                    <p>👤 ${user.following} Seguindo</p>
+                                                </div>
+                                        </div>`
 
         let repositoriesItems = ''
-        user.repositories.forEach(repo => repositoriesItems += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a> <ul class="status"><li>🍴 ${repo.forks}</li> <li>⭐ ${repo.stargazers_count}</li> <li>👀 ${repo.watchers}</li> <li>👨‍💻 ${repo.language}</li></ul></li>`);
+        user.repositories.forEach(repo => repositoriesItems += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                                                                    <ul class="status">
+                                                                        <li>🍴 ${repo.forks}</li>
+                                                                        <li>⭐ ${repo.stargazers_count}</li>
+                                                                        <li>👀 ${repo.watchers}</li> <li>👨‍💻 ${repo.language}</li>
+                                                                    </ul>
+                                                                </li>`);
         
         if(user.repositories.length > 0){
-            this.userProfile.innerHTML += `<div class="repositories section">
-            <h2>Repositórios</h2>
-            <ul>${repositoriesItems}</ul>
-            </div>`
+            this.userProfile.innerHTML += ` <div class="repositories section">
+                                                <h2>Repositórios</h2>
+                                                <ul>${repositoriesItems}</ul>
+                                            </div>`
         }
 
         let eventsItems = ''
@@ -27,8 +33,8 @@ const screen = {
         })
 
         if(user.events.length > 0) {
-            this.userProfile.innerHTML += `<h2>Eventos:</h2>
-            <ul class="list-events">${eventsItems}</ul>`
+            this.userProfile.innerHTML += ` <h2>Eventos:</h2>
+                                            <ul class="list-events">${eventsItems}</ul>`
         }
     },
     renderNotFound(){
