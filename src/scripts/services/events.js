2dc885406679e4f941fc21d0fd76ce1c13332f03
moizzito@gmail.com <moizzito@gmail.com>
2023-08-02 21:40:12 -0300

    Criei PushEvent e CreatEvent.

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -2,7 +2,8 @@ import { baseUrl, quantity } from "../variables.js";
 
 async function getEvents(userName) {
     const response = await fetch(`${baseUrl}/${userName}/events?per_page=${quantity}`)
-    return await response.json()
+    const eventos = await response.json()
+    return eventos.filter(elemento => {return elemento.type === 'PushEvent' || elemento.type === 'CreateEvent'})
 }
 
 export {getEvents}
