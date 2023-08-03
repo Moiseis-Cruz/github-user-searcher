9b8cb28540140e49b282f52191b5019393f2766f
moizzito@gmail.com <moizzito@gmail.com>
2023-08-03 20:57:39 -0300

    Utilizei slice e filter para buscar os eventos.

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -1,14 +1,14 @@
 import { baseUrl, quantity } from "../variables.js";
 
-// async function getEvents(userName) {
-//     const response = await fetch(`${baseUrl}/${userName}/events`)
-//     const events = await response.json()
-//     return events.filter(elements => {return elements.type === 'PushEvent' || elements.type === 'CreateEvent'}).slice(0, quantity)
-// }
-
 async function getEvents(userName) {
-    const response = await fetch(`${baseUrl}/${userName}/events?per_page=${quantity}`)
-    return await response.json()
+    const response = await fetch(`${baseUrl}/${userName}/events`)
+    const events = await response.json()
+    return events.filter(elements => {return elements.type === 'PushEvent' || elements.type === 'CreateEvent'}).slice(0, quantity)
 }
+
+// async function getEvents(userName) {
+//     const response = await fetch(`${baseUrl}/${userName}/events?per_page=${quantity}`)
+//     return await response.json()
+// }
 
 export {getEvents}
